91e4dd6848885e23fd7e24a69fba97da9564c389
Vegard Solheim <vegard.solheim@inmeta.no>
2018-03-15 14:33:51 +0100

    Added PresentationModel and PresentationController with its useful methods

---
+++ AwkwardPresentationBackend/AwkwardPresentationBackend/Controllers/PresentationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AwkwardPresentationBackend.Controllers
+{
+    public class PresentationController : ApiController
+    {
+        public int StartSession()
+        {
+            // return a new session ID for a new presentation
+            return 0;
+        }
+        
+
+        public string GetSlideData(int id)
+        {
+            // return the latest slide
+            return "text";
+        }
+
+        public void UploadSlideData(int id, string url, string text)
+        {
+            // Add this text to 
+        }
+    }
+}
---
+++ AwkwardPresentationBackend/AwkwardPresentationBackend/Models/PresentationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AwkwardPresentationBackend.Models
+{
+    public class PresentationModel
+    {
+        public int Id { get; set; }
+
+        public IList<string> PresentationText { get; set; }
+        public IList<string> Urls { get; set; }
+    }
+
+}
