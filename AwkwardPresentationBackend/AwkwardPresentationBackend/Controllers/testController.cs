90f8dc279b14f70bfc774f150eeb5a960877ce70
Jon Anders Sylvarnes <jon.anders.sylvarnes@inmeta.no>
2018-03-15 17:56:25 +0100

    ImageProviderService and test controller

---
+++ AwkwardPresentationBackend/AwkwardPresentationBackend/Controllers/testController.cs
@@ -0,0 +1,44 @@
+﻿using AwkwardPresentationBackend.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AwkwardPresentationBackend.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpPost]
+        public async Task<ActionResult> Test()
+        {
+            string a = "";
+            using (var reader = new StreamReader(Request.InputStream))
+                a = reader.ReadToEnd();
+            if (a != null)
+                return new JsonResult(){
+                    Data = new { Result = "Yay!" },
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            else
+                return new JsonResult()
+                {
+                    Data = new { Result = "Aaw!" },
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+        }
+
+        public async Task<ActionResult> RunTest()
+        {
+            var data = await ImageProvider.RunAsync("");
+            return new JsonResult()
+            {
+                Data = new { Result = data},
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            }; 
+        }
+    }
+}
---
+++ AwkwardPresentationBackend/AwkwardPresentationBackend/Services/ImageProvider.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+
+namespace AwkwardPresentationBackend.Services
+{
+    public class ImageProvider
+    {
+        static HttpClient client = new HttpClient();
+
+        public static async Task<string> RunAsync(string searchText)
+        {
+            var response = await client.PostAsJsonAsync("http://placeholder.no/test/test", "Yay!");
+            string result = "";
+            if (response.IsSuccessStatusCode)
+            {
+                var a = await response.Content.ReadAsStringAsync();
+                dynamic b = JsonConvert.DeserializeObject(a);
+                if (b != null && b.Result != null)
+                    result = b.Result;
+            }
+            return result;
+        } 
+    }
+}
