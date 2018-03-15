4aa5cdf4bbe9b6d01dbfc97883e1244b54ecd6a3
Vegard Solheim <vegard.solheim@inmeta.no>
2018-03-15 16:35:10 +0100

    Added database connection

--- AwkwardPresentationBackend/AwkwardPresentationBackend/Controllers/HomeController.cs
+++ AwkwardPresentationBackend/AwkwardPresentationBackend/Controllers/HomeController.cs
@@ -3,14 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AwkwardPresentationBackend.Models;
 
 namespace AwkwardPresentationBackend.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public string Index()
         {
-            return View();
+            string test2 = "";
+
+            using (var db = new DatabaseContext())
+            {
+                var presentation = new PresentationModel()
+                {
+                    PresentationText = new List<string>()
+                    {
+                        "string one"
+                    }
+                };
+
+                db.Presentations.Add(presentation);
+                db.SaveChanges();
+
+                var test3 = db.Presentations.Select(p => p);
+
+                var test = from p in db.Presentations
+                    select p;
+
+                foreach (var testItem in test)
+                {
+
+                    if (testItem != null && testItem.PresentationText != null)
+                    {
+                        foreach (var str in testItem.PresentationText)
+                        {
+
+                            test2 = test2 + str;
+                        } 
+                    }
+                }
+            }
+
+
+            return test2;
+            //return View();
         }
 
         public ActionResult About()
