a14ba9683fcb5ff2742cbf9a7a03a4e71b7a10d5
Vegard Solheim <vegard.solheim@inmeta.no>
2018-03-15 13:01:40 +0100

    Initial commit

---
+++ AwkwardPresentationBackend/AwkwardPresentationBackend/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AwkwardPresentationBackend.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ AwkwardPresentationBackend/AwkwardPresentationBackend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AwkwardPresentationBackend.WebApiApplication" Language="C#" %>
